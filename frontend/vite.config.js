f423d072b0745cff1e7ff6932863b714a3500a9d
Bibin Benny Peter <bibinbennypeter@gmail.com>
2024-11-05 22:58:47 +0530

    chore: update project dependencies and configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    '/api':{
+    target: 'http://localhost:8000'
+    }
+  },
   plugins: [react()],
 })
